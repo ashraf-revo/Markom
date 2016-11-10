5d5bb053977c646e22e6d2cb4192dfa83162ad49
ashraf <ashraf1abdelrasool@gmail.com>
2016-11-10 13:28:34 +0200

    Initial commit

---
+++ src/test/java/org/revo/MarkomApplicationTests.groovy
@@ -0,0 +1,14 @@
+package org.revo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class MarkomApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
